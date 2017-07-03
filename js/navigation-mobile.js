5a8a4abc1c5931e384dcc9dba70159c20a4937c4
Shully <hacerqwett@gmail.com>
2017-07-03 23:50:44 +0300

    Mobile dropdown now toggles on click, not on hover

--- js/navigation-mobile.js
+++ js/navigation-mobile.js
@@ -2,5 +2,15 @@ $(document).ready(function () {
     $("div.icon-list-wrapper").click(function () {
         $("nav").toggleClass("nav-stretch");
         $("ul.nav-list").toggleClass("switch-mobile");
+    });
+
+    $("a.nav-item-link, li.nav-subitem").click(function () {
+        $("a.nav-item-link, li.nav-subitem").removeClass("mobile-link-hovered");
+        $(this).toggleClass("mobile-link-hovered")
+    });
+
+    $("a.home-link").click(function () {
+        $("ul.nav-sublist").toggleClass("toggle-sublist");
+        $("i.home-icon").toggleClass("mobile-icon-rotate");
     })
 });
