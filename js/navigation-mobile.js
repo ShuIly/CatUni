a02137a87b96debf275032ba808e4d7db2736ba6
Shully <hacerqwett@gmail.com>
2017-07-04 15:31:14 +0300

    Removed unneeded media screens. Cleaned some code

--- js/navigation-mobile.js
+++ js/navigation-mobile.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     $("div.icon-list-wrapper").click(function () {
         $("nav").toggleClass("nav-stretch");
         $("ul.nav-list").toggleClass("switch-mobile");
+        $("ul.nav-sublist").removeClass("toggle-sublist");
+        $("a.nav-item-link, li.nav-subitem").removeClass("mobile-link-hovered");
     });
 
     $("a.nav-item-link, li.nav-subitem").click(function () {
